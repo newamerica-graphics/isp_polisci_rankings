18cefec7a6deb7de2021de00b5235faac9cf4ddd
lorenries <lorenries@users.noreply.github.com>
2018-10-19 16:30:30 -0400

    add chart IDs

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = env => {
       env.deploy === "development" && new webpack.HotModuleReplacementPlugin(),
       new HtmlWebpackPlugin({
         title: "",
-        chartIDs: [],
+        chartIDs: ["viz__1", "viz__heatmap"],
         inject: false,
         template: path.resolve(__dirname, "src/index.html")
       }),
