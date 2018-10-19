7c9cda7e4f4934464a3f58d0aba334896d325f96
lorenries <lorenries@users.noreply.github.com>
2018-10-19 16:30:53 -0400

    start arrow plot

---
+++ src/charts/ArrowPlot/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class ArrowPlot extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div />
+      </div>
+    );
+  }
+}
